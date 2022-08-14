aba4a48a11968ef1527a6d54c3d9840f9917e8e4
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-08-14 00:55:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+ Title: app.js
+ Author: Professor Krasso
+ Date: 08/14/2022
+ Modified By: Anita Taylor
+ Description: Intro to RESTful api
+*/
+
+const express = require("express");
+const http = require("http");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const mongoose = require("mongoose");
+
+/* Initialize Express */
+var app = express();
+
+/* Set app port to connect to port 3000  */
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Doc: define an object literal named options with the following properties/values
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "WEB 420 RESTful APIs",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/*.js"], // files containing annotations for the OpenAPI Specification
+};
+
+/* Doc: call the swaggerJsdoc library using the options object literal. */
+const openapiSpecification = swaggerJsdoc(options);
+/* Doc: wire the openapiSpecification variable to the app variable. Configure express to use /api-docs route to serve swaggerJsdoc  */
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+/* Doc: use the http library to create a new server that listens on the port 3000  */
+http.createServer(app).listen(app.get("port"), function () {
+  console.log(`Application started and listening on port ${app.get("port")}`);
+});
