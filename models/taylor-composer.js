72658a9c70de63585060c604ac299b8c1abbc4b6
Anita L Taylor <98141576+aniltaylor@users.noreply.github.com>
2022-09-28 23:51:47 -0500

    Update taylor-composer.js

--- models/taylor-composer.js
+++ models/taylor-composer.js
@@ -1,1 +1,14 @@
+// Title: taylor-composer.js
+// Author: anita taylor
+// Date: 09/21/2022
+// Description: Mongoose model for composer API
 
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+let composerSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Composer", composerSchema)
